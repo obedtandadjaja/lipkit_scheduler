ed9ca6dd6098c318cc19c79b40db0434a0ac9c0a
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2016-03-24 02:33:28 -0400

    added from

--- app/mailers/mailgun_mailer.rb
+++ app/mailers/mailgun_mailer.rb
@@ -4,7 +4,9 @@ class MailgunMailer < ApplicationMailer
   # with the following lookup:
   #
   #   en.mailgun_mailer.lipkit_notification.subject
-  #
+  
+  default from: 'obed.tandadjaja@covenant.edu'
+
   def lipkit_notification(lipkit)
     mail to: "obed.tandadjaja@gmail.com", subject: "#{lipkit} Lipkit Available"
   end
