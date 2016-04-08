ddc77fd725b736d8c6a77e8cce11cad39671d1ff
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2016-04-08 13:15:46 -0400

    renamed app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_lipkit_scheduler_session'
+Rails.application.config.session_store :cookie_store, key: '_scheduler_session'
